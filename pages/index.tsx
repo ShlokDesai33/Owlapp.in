a340b3b5bc33d3121704df23fc312d89999a63b9
shlokdesai <shlokdesai33@gmail.com>
2022-11-22 15:21:37 -0600

    index changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { List, X } from 'phosphor-react'
+import Link from 'next/link'
 import Image from 'next/image'
 import logoSvg from '../public/images/logo.svg'
-import { useState } from 'react'
 import Spinner from '../components/lib/spinner'
-import Link from 'next/link'
-import { ArrowRight } from 'phosphor-react'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { NextPage } from 'next/types'
+
+const navigation = [
+  { name: 'About Us', href: '/about' },
+  { name: 'Our Vision', href: '/vision' },
+  { name: 'Terms & Conditions', href: '/terms&conditions' },
+  { name: 'Privacy Policy', href: '/policy' },
+]
 
 /**
  * Landing page for the app
  * @returns {NextPage} next page without layout
  */
 const Home: NextPage = () => {
-  // set to loading when user clicks log in/sign up
+  // define states
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>('');
   const router = useRouter();
 
   if (loading) {
@@ -48,109 +56,217 @@ const Home: NextPage = () => {
         <link rel="icon" href="/images/favicon.ico"/>
       </Head>
 
-      <main>
-        <div className="flex flex-col h-screen w-screen bg-landing bg-cover">
-          <div className="flex items-center justify-between mt-14 mx-24">
-            <div className="flex items-center">
-              <Image
-                src={logoSvg}
-                width={60}
-                height={60}
-                alt="Owl logo"
-              />
-              <h2 className="ml-3">Owl</h2>
-            </div>
-
-            <div className="absolute flex items-center justify-center gap-x-10 left-0 right-0">
-              <Link href="/about" passHref>
-                <button className="text-xxl">About Us</button>
-              </Link>
-
-              <Link href="/vision" passHref>
-                <button className="text-xxl">Our Vision</button>
-              </Link>
-
-              <Link href="/contact" passHref>
-                <button className="text-xxl">Contact Us</button>
-              </Link>
-            </div>
-
-            <button className="group border-2 border-black px-7 py-2 rounded-2xl hover:border-primary z-10" onClick={(e) => {
-              e.preventDefault();
-              setLoading(true);
-              router.push('/auth/signin');
-            }}>
-              <h4 className="group-hover:text-primary">Log in</h4>
-            </button>
-          </div>
-
-          <div className="flex items-center h-full justify-center">
-            <div className="flex divide-x-2 justify-center mb-10">
-              <div className="flex flex-col my-10 mr-20">
-                <h1>Find and rent scientific<br/>resources on <span className="text-white header-stroke font-extrabold">Owl</span></h1>
-                <h4 className="text-gray-text mt-6">
-                  Owl makes it exceptionaly easy to book scientific and<br/>
-                  research oriented resources offered by universities, colleges<br/>
-                  and private organizations. 
-                </h4>
-
-                <h5 className="font-medium mt-14 mb-7">Got questions? Send us a message!</h5>
-                <div className="flex items-center justify-between border-2 px-6 py-4 rounded-2xl border-gray-btn mr-20">
-                  <input
-                    type="text"
-                    placeholder="Enter your email address..."
-                    className="outline-none bg-transparent text-xxl placeholder:text-xxl placeholder:text-gray-text w-full mr-6"
-                    onChange={(e) => {setEmail(e.target.value)}}
+      <div className="isolate bg-white h-screen">
+        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+          <svg
+            className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+            viewBox="0 0 1155 678"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+              fillOpacity=".3"
+              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+            />
+            <defs>
+              <linearGradient
+                id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                x1="1155.49"
+                x2="-78.208"
+                y1=".177"
+                y2="474.645"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#9089FC" />
+                <stop offset={1} stopColor="#FF80B5" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <div className="px-6 pt-6 lg:px-8">
+          <div>
+            <nav className="flex h-9 items-center justify-between" aria-label="Global">
+              <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
+                <div className="-m-1.5 p-1.5">
+                  <span className="sr-only">Owl</span>
+                  <Image
+                    src={logoSvg}
+                    alt="Logo"
+                    width={50}
+                    height={50}
                   />
-
-                  <button disabled={!email} onClick={(e) => {
-                    e.preventDefault();
-                    setLoading(true);
-                    router.push(`/contact?email=${encodeURIComponent(email)}`);
-                  }}>
-                    <ArrowRight weight="regular" className="text-primary" size={40} />
-                  </button>
                 </div>
               </div>
-
-              <div className="flex flex-col justify-center divide-y-2 pl-10">
-                <div className="flex flex-col gap-y-6 pb-10">
-                  <h3>Looking to rent or learn more?</h3>
-                  <button className="flex items-center gap-x-2 w-fit" onClick={(e) => {
+              <div className="flex lg:hidden">
+                <button
+                  type="button"
+                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(true)}
+                >
+                  <span className="sr-only">Open main menu</span>
+                  <List size={30} className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+                {navigation.map((item) => (
+                  <Link key={item.name} href={item.href} passHref>
+                    <button className="font-semibold text-gray-900 hover:text-gray-900 shrink-0 hover:underline underline-offset-2">
+                      {item.name}
+                    </button>
+                  </Link>
+                ))}
+              </div>
+              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+                <button 
+                  className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                  onClick={e => {
                     e.preventDefault();
                     setLoading(true);
-                    router.push('/auth/signup');
-                  }}>
-                    <h4 className="text-primary">Sign up for Owl</h4>
-                    <ArrowRight weight="regular" className="text-primary" size={35} />
-                  </button>
-                </div>
-                
-                <div className="flex flex-col gap-y-6 pt-10">
-                  <h3>Or, register as an admin today!</h3>
-                  <a className="flex items-center gap-x-2 w-fit" href="https://owl-console.vercel.app">
-                    <h4 className="text-primary">Register today</h4>
-                    <ArrowRight weight="regular" className="text-primary" size={35} />
-                  </a>
-
-                  <h5 className="text-gray-text">By registering yourself as an admin, you and your<br/>organization agree to our
-                    <Link href="/terms&conditions" passHref>
-                      <button className="underline underline-offset-2 ml-1">Terms and Conditions</button>
-                    </Link>.
-                  </h5>
-                </div>
+                    router.push('/auth/signin');
+                  }}
+                >
+                  Log in
+                </button>
               </div>
-            </div>
-          </div>
-
-          <div className="flex items-center px-24 pb-16 justify-between">
-            <h4>{'// Polymath Tech'}</h4>
-            <h4>©Owl 2022 all rights reserved</h4>
+            </nav>
+            <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+              <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+                <div className="flex h-9 items-center justify-between">
+                  <div className="flex">
+                    <div className="-m-1.5 p-1.5">
+                      <span className="sr-only">Owl</span>
+                      <Image
+                        src={logoSvg}
+                        alt="Logo"
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <button
+                      type="button"
+                      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <X size={30} className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-6 flow-root">
+                  <div className="-my-6 divide-y divide-gray-500/10">
+                    <div className="space-y-2 py-6">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                    <div className="py-6">
+                      <div 
+                        className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                        onClick={e => {
+                          e.preventDefault();
+                          setLoading(true);
+                          router.push('/auth/signin');
+                        }}
+                      >
+                        Log in
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Dialog>
           </div>
         </div>
-      </main>
+        <main className="h-full pb-20">
+          <div className="relative px-6 lg:px-8 h-full">
+            <div className="mx-auto max-w-3xl h-full flex items-center">
+              <div>
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                  <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                    <span className="text-gray-600">
+                      Admin or Organization?{' '}
+                      <a href="https://owl-console.vercel.app/" className="font-semibold text-primary">
+                        <span className="absolute inset-0" aria-hidden="true" />
+                        Go to console<span aria-hidden="true">&rarr;</span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                    Find and Rent Scientific Resources at <span className="text-white header-stroke font-extrabold tracking-wide sm:tracking-normal">Owl</span>
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                    We make it exceptionaly easy to book scientific and research oriented
+                    resources offered by universities, colleges and private organizations. 
+                  </p>
+                  <div className="mt-8 flex gap-x-4 sm:justify-center">
+                    <button
+                      className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+                      onClick={e => {
+                        e.preventDefault();
+                        setLoading(true);
+                        router.push('/auth/signup');
+                      }}
+                    >
+                      Get started
+                      <span className="text-indigo-200" aria-hidden="true">
+                        &rarr;
+                      </span>
+                    </button>
+                    <Link href="/resources" passHref>
+                      <button className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                        Browse
+                        <span className="text-gray-400" aria-hidden="true">
+                          &rarr;
+                        </span>
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
+          <svg
+            className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+            viewBox="0 0 1155 678"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+              fillOpacity=".3"
+              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+            />
+            <defs>
+              <linearGradient
+                id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+                x1="1155.49"
+                x2="-78.208"
+                y1=".177"
+                y2="474.645"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#9089FC" />
+                <stop offset={1} stopColor="#FF80B5" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+      </div>
     </>
-  );
+  )
 }
 
 export default Home
