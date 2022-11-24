08334863788d68af89b7b0d8877c202fea44f667
shlokdesai <shlokdesai33@gmail.com>
2022-11-24 17:49:15 -0600

    css tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,16 @@
 import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { List, X } from 'phosphor-react'
 import Link from 'next/link'
-import Image from 'next/image'
-import logoSvg from '../public/images/logo.svg'
 import Spinner from '../components/lib/spinner'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { NextPage } from 'next/types'
-
-const navigation = [
-  { name: 'About Us', href: '/about' },
-  { name: 'Our Vision', href: '/vision' },
-  { name: 'Terms & Conditions', href: '/terms&conditions' },
-  { name: 'Privacy Policy', href: '/policy' },
-]
+import MainNavbar from '../components/layout/components/navbar'
 
 /**
  * Landing page for the app
  * @returns {NextPage} next page without layout
  */
 const Home: NextPage = () => {
-  // define states
-  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
 
@@ -56,7 +44,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/images/favicon.ico"/>
       </Head>
 
-      <div className="isolate bg-white h-screen">
+      <div className="isolate bg-white h-full pb-20">
         <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
           <svg
             className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
@@ -84,108 +72,9 @@ const Home: NextPage = () => {
             </defs>
           </svg>
         </div>
-        <div className="px-6 pt-6 lg:px-8">
-          <div>
-            <nav className="flex h-9 items-center justify-between" aria-label="Global">
-              <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
-                <div className="-m-1.5 p-1.5">
-                  <span className="sr-only">Owl</span>
-                  <Image
-                    src={logoSvg}
-                    alt="Logo"
-                    width={50}
-                    height={50}
-                  />
-                </div>
-              </div>
-              <div className="flex lg:hidden">
-                <button
-                  type="button"
-                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                  onClick={() => setMobileMenuOpen(true)}
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <List size={30} className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
-                {navigation.map((item) => (
-                  <Link key={item.name} href={item.href} passHref>
-                    <button className="font-semibold text-gray-900 hover:text-gray-900 shrink-0 hover:underline underline-offset-2">
-                      {item.name}
-                    </button>
-                  </Link>
-                ))}
-              </div>
-              <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
-                <button 
-                  className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                  onClick={e => {
-                    e.preventDefault();
-                    setLoading(true);
-                    router.push('/auth/signin');
-                  }}
-                >
-                  Log in
-                </button>
-              </div>
-            </nav>
-            <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-              <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
-                <div className="flex h-9 items-center justify-between">
-                  <div className="flex">
-                    <div className="-m-1.5 p-1.5">
-                      <span className="sr-only">Owl</span>
-                      <Image
-                        src={logoSvg}
-                        alt="Logo"
-                        width={50}
-                        height={50}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex">
-                    <button
-                      type="button"
-                      className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                      onClick={() => setMobileMenuOpen(false)}
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <X size={30} className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                </div>
-                <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                    <div className="py-6">
-                      <div 
-                        className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                        onClick={e => {
-                          e.preventDefault();
-                          setLoading(true);
-                          router.push('/auth/signin');
-                        }}
-                      >
-                        Log in
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Dialog>
-          </div>
-        </div>
+        
+        <MainNavbar />
+        
         <main className="h-full pb-20">
           <div className="relative px-6 lg:px-8 h-full">
             <div className="mx-auto max-w-3xl h-full flex items-center">
@@ -237,6 +126,7 @@ const Home: NextPage = () => {
             </div>
           </div>
         </main>
+
         <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]">
           <svg
             className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+import Navbar from "../components/layout/components/navbar"
+
+export default function Test() {
+  return (
+    <Navbar />
+  )
+}
